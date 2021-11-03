f09e6e471a196fd161b7513ea1263b0f11f72f23
Kim Brannian <brannian@gmail.com>
2021-11-02 20:39:15 -0700

    Create new logic for scripts
    Co-Authored-by: Matt Luker <jamesmattluker@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,70 @@
+function Ticket(movie, cost, time, age) {
+  this.movie = movie;
+  this.time = time;
+  this.age = age;
+  this.cost = cost;
+} 
+
+Ticket.prototype.costAssign = function() {
+  if (this.movie = "shrek" || "black widow") {
+    this.cost = 12
+  } else {
+    this.cost = 10
+  }
+}
+
+
+
+/*
+time === matinee $8
+time ==== regular cost of movie
+ 
+if regular cost then
+
+age === child - $4
+age === reg adult $0
+age === senior -$5
+
+
+if time === matinee;
+  cost = $8
+  else cost = this.cost
+
+if age === senior;
+  cost = this.cost - $5
+  else if age === child
+    cost = this.cost - $4
+    else 
+      return this.cost
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
